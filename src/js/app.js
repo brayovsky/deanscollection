07d5fad4c3992dcac364ec5544d65b8d8bbd03a1
Brian Muthui <brian.muthui@andela.com>
2017-12-24 21:03:04 +0300

    Use routers

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import clothesStoreApp from './reducers';
-import HomeSplash from './components/HomeSplash';
+import Root from './components/Root'
 
 let store = createStore(clothesStoreApp);
 
+const areWeIn = () => console.log('in');
+
 ReactDOM.render(
   <Provider store={store}>
-    <HomeSplash />
+    <BrowserRouter>
+      <div>
+        <Route path='/' component={Root} componentDidMount={areWeIn}/>
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
