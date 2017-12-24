c4de4c8bd7a7302175469b76e771574492798301
Brian Muthui <brian.muthui@andela.com>
2017-12-24 20:57:11 +0300

    Add router to reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import categories from './categories';
 import pages from './pages';
+import { routerReducer } from 'react-router-redux';
 
 const clothesStoreApp = combineReducers({
   categories,
   pages,
+  routing: routerReducer,
 });
 
 export default clothesStoreApp;
