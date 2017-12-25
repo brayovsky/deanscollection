4505ab92856f7016cf188c2cf8f2fdabb789c1d9
Brian Muthui <brian.muthui@andela.com>
2017-12-25 11:59:10 +0300

    Refactor component - Make responsive - Change name - Separate styles

--- src/js/components/ClothesGrid.js
+++ src/js/components/ClothesGrid.js
@@ -1,32 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
+import styles from '../styles/clothesGridStyle';
 import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
 
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    height: 140,
-    width: 100,
-  },
-  control: {
-    padding: theme.spacing.unit * 2,
-  },
-});
-
-class GuttersGrid extends React.Component {
+class ClothesGrid extends React.Component {
   constructor(props){
     super(props);
   }
-// 
-//   handleChange = key => (event, value) => {
-//     this.setState({
-//       [key]: value,
-//     });
-//   };
 
   render() {
     const { classes } = this.props;
@@ -34,10 +16,12 @@ class GuttersGrid extends React.Component {
     return (
       <Grid container className={classes.root}>
         <Grid item xs={12}>
-          <Grid container className={classes.demo} justify="center" spacing={Number(16)}>
+          <Grid container justify="center" spacing={Number(16)}>
             {[0, 1, 2].map(value => (
-              <Grid key={value} item>
-                <Paper className={classes.paper} />
+              <Grid key={value} item xs={12} sm={3}>
+                <Paper className={classes.paper} title="Whats up!">
+                  <p>This is content exceeding width and other stuff. No width in css</p>
+                </Paper>
               </Grid>
             ))}
           </Grid>
@@ -47,8 +31,8 @@ class GuttersGrid extends React.Component {
   }
 }
 
-GuttersGrid.propTypes = {
+ClothesGrid.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(GuttersGrid);
+export default withStyles(styles)(ClothesGrid);
