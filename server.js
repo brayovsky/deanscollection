c7a97d133c5ed67ff3ab8469f4d4af87c38d98ea
Brian Muthui <brian.muthui@andela.com>
2017-12-25 19:10:11 +0300

    Use express.js to start local server

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.use('/build', express.static('./build'));
+app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
+
+app.listen(3000, () => console.log('Deans collection listening on port 3000!'));
