797d722672697423c27d4d17ecb078de1ebf6c8a
Brian Muthui <brian.muthui@andela.com>
2017-12-27 22:50:24 +0300

    Add changeaction

--- src/js/components/Categories.js
+++ src/js/components/Categories.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'material-ui/Button';
 import { withStyles } from 'material-ui/styles';
-import Dialog, { DialogActions, DialogContent, DialogTitle } from 'material-ui/Dialog';
 import Input, { InputLabel } from 'material-ui/Input';
 import { MenuItem } from 'material-ui/Menu';
 import { FormControl, FormHelperText } from 'material-ui/Form';
@@ -10,28 +9,21 @@ import Select from 'material-ui/Select';
 import { connect } from 'react-redux';
 import styles from '../styles/categoryStyle'
 import { CircularProgress } from 'material-ui/Progress';
+import { activeCategoryChanged } from '../actions/actions'
+import Typography from 'material-ui/Typography/Typography';
 
 class Categories extends React.Component {
   constructor(props){
     super(props);
-
     this.state = {
       categoryValue: 'all',
-      dialogOpen: false,
       isLoading: false,
     }
   }
 
-  handleChange = name => event => {
-    this.setState({ [name]: event.target.value });
-  };
-
-  handleClickOpen = () => {
-    this.setState({ dialogOpen: true });
-  };
-
-  handleClose = () => {
-    this.setState({ dialogOpen: false });
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.name });
+    this.props.onChangeCategory(event.target.value);
   };
 
   showLoadingIcon(){
@@ -42,58 +34,57 @@ class Categories extends React.Component {
     );
   }
 
+  getCategoryName(id) {
+    if (id === 'all' || id === 'undefined'){
+      return 'all';
+    }
+    const activeCategory = this.props.categories.filter((category) => {
+      if(String(category.id) === id){
+        return true
+      }
+    });
+    console.log('id is ', id)
+    console.log('activecat', activeCategory);
+    return activeCategory[0].name;
+  }
+
   showCategoriesDropdown(){
     const { classes } = this.props;
     return (
+      <div>
       <form className={classes.container}>
         <FormControl className={classes.formControl}>
           <InputLabel htmlFor="cat-inp">Category</InputLabel>
           <Select
             value={this.state.categoryValue}
-            onChange={this.handleChange('categoryValue')}
-            input={<Input id="cat-inp" />}
+            onChange={this.handleChange}
+            input={<Input name="categoryValue" id="cat-inp" />}
             disabled={this.props.errorFetching}
             error={this.props.errorFetching}
-            renderValue={value => this.props.errorFetching ? 'Error!' : value }
+            renderValue={value => this.props.errorFetching ? 'Error!' : 'Pick a category' }
           >
-            <MenuItem key='all' value={'all'}>All</MenuItem>
+            <MenuItem key='all' value="all">All</MenuItem>
             {
-              this.props.categories.map(category => <MenuItem key={category.id} value={category.slug}>{category.name}</MenuItem>)
+              this.props.categories.map(category => <MenuItem key={category.id} value={category.id}>{category.name}</MenuItem>)
             }
           </Select>
           <FormHelperText>{this.props.errorFetching ? 'Please check network connection' : 
           'Re-click to unselect a category' }</FormHelperText>
         </FormControl>
       </form>
+      <Typography type="display2">
+        Now showing {this.getCategoryName(this.props.activeCategoryId)}
+      </Typography>
+      </div>
     );
   }
 
   render() {
-
     return (
       <div>
-        <Button onClick={this.handleClickOpen}>Choose a category</Button>
-        <Dialog
-          ignoreBackdropClick
-          ignoreEscapeKeyUp
-          open={this.state.dialogOpen}
-          onClose={this.handleClose}
-        >
-          <DialogTitle>Choose a category</DialogTitle>
-          <DialogContent>
           {
             this.props.isFetching ? this.showLoadingIcon() : this.showCategoriesDropdown()
           }  
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={this.handleClose} color="primary">
-              Ok
-            </Button>
-          </DialogActions>
-        </Dialog>
       </div>
     );
   }
@@ -102,18 +93,28 @@ class Categories extends React.Component {
 Categories.propTypes = {
   categories: PropTypes.arrayOf(PropTypes.object).isRequired,
   classes: PropTypes.object.isRequired,
+  onChangeCategory: PropTypes.func,
+  activeCategoryId: PropTypes.string.isRequired,
 };
   
 Categories.defaultProps = {
   categories: [{}],
+  activeCategoryId: 'all',
 };
   
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onChangeCategory: (newCategory) => {dispatch(activeCategoryChanged(newCategory))}
+  }
+};
+
 const mapStateToProps = (state) => {
   return {
     categories: state.categories.allCategories,
     isFetching: state.categories.isFetching,
     errorFetching: state.categories.errorFetching,
+    activeCategoryId: String(state.posts.activeCategory),
   };
 };
 
-export default withStyles(styles)(connect(mapStateToProps)(Categories));
+export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(Categories));
