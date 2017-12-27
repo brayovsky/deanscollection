517020f14adb696ba6b872bc6f4c70ce5f1fc049
Brian Muthui <brian.muthui@andela.com>
2017-12-27 14:30:59 +0300

    Build simple component

--- src/js/components/ClothesGrid.js
+++ src/js/components/ClothesGrid.js
@@ -4,6 +4,7 @@ import { withStyles } from 'material-ui/styles';
 import styles from '../styles/clothesGridStyle';
 import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
+import { connect } from 'react-redux';
 
 class ClothesGrid extends React.Component {
   constructor(props){
@@ -11,16 +12,19 @@ class ClothesGrid extends React.Component {
   }
 
   render() {
+    console.log(this.props);
     const { classes } = this.props;
+    const { posts } = this.props;
+    console.log('posts is ', posts)
 
     return (
-      <Grid container className={classes.root}>
+      <Grid container alignItems="center" className={classes.root}>
         <Grid item xs={12}>
           <Grid container justify="center" spacing={Number(16)}>
-            {[0, 1, 2].map(value => (
-              <Grid key={value} item xs={12} sm={3}>
-                <Paper className={classes.paper} title="Whats up!">
-                  <p>This is content exceeding width and other stuff. No width in css</p>
+            { posts.map(post => (
+              <Grid key={post.id} item xs={12} sm={4}>
+                <Paper className={classes.paper} title={post.title.rendered}>
+                  <p>{ post._links['wp:featuredmedia'][0].href }</p>
                 </Paper>
               </Grid>
             ))}
@@ -33,6 +37,19 @@ class ClothesGrid extends React.Component {
 
 ClothesGrid.propTypes = {
   classes: PropTypes.object.isRequired,
+  posts: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-export default withStyles(styles)(ClothesGrid);
+ClothesGrid.defaultProps = {
+  posts: [{id:'fake', title: {rendered: 'title'}, _links: { 'wp:featuredmedia': 'heyall' }}],
+};
+
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts.posts,
+    isFetching: state.posts.isFetching,
+    errorFetching: state.posts.errorFetching,
+  };
+};
+
+export default withStyles(styles)(connect(mapStateToProps)(ClothesGrid));
