2dd204a7f2f520db6667e1c83c8c42a1daaa5696
Brian Muthui <brian.muthui@andela.com>
2017-12-27 22:51:37 +0300

    Refactor

--- src/js/utils/lib/adapters/apiAdapter.js
+++ src/js/utils/lib/adapters/apiAdapter.js
@@ -1,16 +1,13 @@
-const _ = require('lodash/fp/object');
-
 export const mapJsonToUrlParams = (params) => {
   if (typeof params !== 'object'){
     return '';
   }
-  
   let paramQuery = '';
   for (const param in params) {
     if (params.hasOwnProperty(param)) {
-      console.log(`param`);
       paramQuery += `${param}=${params[param]}&`;
     } 
   }
-  return `?${paramQuery}`;
+  // Remove trailing & and add ?
+  return `?${paramQuery.slice(0, paramQuery.length -1)}`;
 };
