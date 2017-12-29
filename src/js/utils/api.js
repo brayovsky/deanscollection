698545e3635bd93a27a08c5360dff3e49497c634
Brian Muthui <brian.muthui@andela.com>
2017-12-29 14:28:19 +0300

    Add status and headers

--- src/js/utils/api.js
+++ src/js/utils/api.js
@@ -2,11 +2,23 @@ import config from '../config';
 import urljoin from 'url-join';
 import { mapJsonToUrlParams } from './lib/adapters/apiAdapter';
 
+const handleResponse = (response) => {
+  let headers = {}
+  response.headers.forEach((value, name) => {
+    headers[name] = value;
+  });
+  return response.json().then((data) => ({
+    status: response.status,
+    body: data,
+    headers,
+  }));
+};
+
 export const callEndpoint = (endpoint, params = null) => {
   let url = endpoint;
   if(params){
     const paramQuery = mapJsonToUrlParams(params);
     url += paramQuery;
   }
-  return fetch(url).then((response) => response.json(), (error) => Error('whoopsy'));
+  return fetch(url).then(handleResponse, (error) => Error('whoopsy'));
 };
