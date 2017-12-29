5fa521955e6d6486afd1a7ade00b844b22021bad
Brian Muthui <brian.muthui@andela.com>
2017-12-29 14:42:52 +0300

    Remove pages reducer reference

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import categories from './categories';
-import pages from './pages';
 import posts from './posts';
 
 const clothesStoreApp = combineReducers({
   categories,
-  pages,
   posts,
 });
 
